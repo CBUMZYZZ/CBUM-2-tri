1894154d187bf508f76e05d9635877752ee0c57a
Sussai <daniel.sussai.martins@escola.pr.gov.br>
2025-07-03 15:48:23 -0300

    asa

--- perguntas.js
+++ perguntas.js
@@ -17,7 +17,19 @@ criaCartao(
 )
 
 criaCartao(
-    'Minecraft',
+    'MINECRAFT',
     'Em qual ano o minecraft foi lancado e quem criou o minecraft?',
     'O Minecraft foi lançado oficialmente em 2011, mas sua versão inicial (alpha) foi lançada em 2009. O jogo foi criado por Markus "Notch" Persson, um desenvolvedor sueco, que fundou a Mojang, a empresa responsável por Minecraft.'
+)
+
+criaCartao(
+    'MINECRAFT'
+    'Qual é a diferença entre o modo "Hardcore" e o modo "Sobrevivência"?'
+    'No modo Hardcore, o jogo é jogado no nível mais difícil (Hard), e, se o jogador morrer, ele perde permanentemente o mundo, sem a possibilidade de respawn. No modo Sobrevivência, você pode morrer e continuar jogando, podendo salvar e carregar o jogo em outra ocasião.'
+)
+
+criaCartao(
+    'Mobs'
+    'Qual mob pode ser montado em Minecraft?'
+    'Alguns mobs que podem ser montados incluem cavalos, burros, lhamas, porcos (com um totem de sela) e até mesmo um Ender Dragon (se você usar um comando ou mods).'
 )
